07e44279a064ef9c16c512161cb477ff63521834
Fcant <fcscanf@outlook.com>
2020-05-08 23:54:07 +0800

    Update-将数据工具类进行分类实现继承和数据共享

---
+++ src/main/java/com/fcant/tools/utils/InitLoadStudentDataInfo.java
@@ -0,0 +1,101 @@
+package com.fcant.tools.utils;
+
+import com.fcant.tools.bean.Student;
+import lombok.Data;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * InitLoadStudentDataInfo
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcant 下午 23:02 2020/5/7/0007
+ */
+@Data
+public class InitLoadStudentDataInfo {
+
+    /**
+     * 名单数据路径
+     */
+    private static final String filePath = "C:\\Users\\fcsca\\OneDrive\\文档\\Excel\\软件161\\B软件161学号姓名名单表.xlsx";
+
+    /**
+     * 学生名单集合
+     */
+    public static final List<Student> STUDENT_LIST = new ArrayList<>();
+
+    /**
+     * 学生学号集合
+     */
+    public static final List<String> STUDENT_NUM_LIST = new ArrayList<>();
+
+    /**
+     * 学生学号为key，学生对象为值的学生表
+     */
+    public static final Map<String, Student> STUDENT_NUM_MAP = new HashMap<>();
+
+    /**
+     * 学生姓名为key，学生对象为值的学生表
+     */
+    public static final Map<String, Student> STUDENT_NAME_MAP = new HashMap<>();
+
+    public static final ReadExcel readExcel = new ReadExcel();
+
+    /**
+     * 项目运行主程序
+     *
+     * @param args 参数
+     * @author Fcant 上午 7:36 2020/5/8/0008
+     */
+    public static void main(String[] args) {
+        initStudentInfo();
+    }
+
+    public static void initStudentInfo() {
+        getExcelStudentInfoData(filePath);
+    }
+
+    /**
+     * 获取Excel中的数据内容
+     *
+     * @param excelPath Excel文件路径
+     * @author Fcant 上午 7:38 2020/5/8/0008
+     */
+    public static void getExcelStudentInfoData(String excelPath) {
+        Workbook wb = readExcel.getExcel(excelPath);
+        List<Student> studentCompany = new ArrayList<>();
+        if (wb == null)
+            System.out.println("文件读入出错");
+        else {
+            Sheet sheet = wb.getSheetAt(0);//读取sheet(从0计数)
+            int rowNum = sheet.getLastRowNum();//读取行数(从0计数)
+            for (int i = 0; i <= rowNum; i++) {
+                Row row = sheet.getRow(i);//获得行
+                int colNum = row.getLastCellNum();//获得当前行的列数
+                Student student = new Student();
+                for (int j = 0; j < colNum; j++) {
+                    Cell cell = row.getCell(j);//获取单元格
+                    if (j == 0) {
+                        String stuNum = cell.toString();
+                        String newNum = "1" + stuNum.substring(2, 11);
+                        student.setStudentNum(newNum.replaceAll("E", "0"));
+                    } else {
+                        student.setStudentName(cell.toString());
+                    }
+                }
+                STUDENT_NUM_LIST.add(student.getStudentNum());
+                STUDENT_NUM_MAP.put(student.getStudentNum(), student);
+                STUDENT_NAME_MAP.put(student.getStudentName(), student);
+                STUDENT_LIST.add(student);
+            }
+        }
+    }
+}
--- src/main/java/com/fcant/tools/utils/StudentFileCommitCount.java
+++ src/main/java/com/fcant/tools/utils/StudentFileCommitCount.java
@@ -2,52 +2,20 @@ package com.fcant.tools.utils;
 
 import com.fcant.tools.bean.Student;
 import org.apache.commons.io.FileUtils;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 
 import java.io.File;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 /**
- * CountNoCommitStudent
+ * StudentFileCommitCount-加载名单表，查看对应的目录下的缺少未交的名单和文件统一规范命名
  * <p>
  * encoding:UTF-8
  *
- * @author Fcant 下午 23:02 2020/5/7/0007
+ * @author Fcant 下午 23:40 2020/5/8/0008
  */
-public class CountNoCommitStudent {
-
-    /**
-     * 名单数据路径
-     */
-    private static final String filePath = "C:\\Users\\fcsca\\OneDrive\\文档\\Excel\\软件161\\B软件161学号姓名名单表.xlsx";
-
-    /**
-     * 对比查找的目录路径
-     */
-    public static final String dirPath = "D:\\LinkSpace\\Work\\WorkFiles\\B软件161\\B软件161-91job就业调查截图";
-
-    /**
-     * 学生名单集合
-     */
-    private static final List<Student> studentList = new ArrayList<>();
-
-    /**
-     * 学生学号集合
-     */
-    private static final List<String> studentNumList = new ArrayList<>();
-
-    /**
-     * 学生学号为key，学生对象为值的学生表
-     */
-    public static final Map<String, Student> STUDENT_NUM_MAP = new HashMap<>();
-
-    /**
-     * 学生姓名为key，学生对象为值的学生表
-     */
-    public static final Map<String, Student> STUDENT_NAME_MAP = new HashMap<>();
+public class StudentFileCommitCount extends InitLoadStudentDataInfo {
 
     /**
      * 保存提交文件的学生的学号
@@ -60,23 +28,13 @@ public class CountNoCommitStudent {
     private static final List<String> commitFileStudentNameList = new ArrayList<>();
 
     /**
-     * 项目运行主程序
-     *
-     * @param args 参数
-     * @author Fcant 上午 7:36 2020/5/8/0008
+     * 对比查找的目录路径
      */
+    private static final String dirPath = "D:\\LinkSpace\\Work\\WorkFiles\\B软件161\\B软件161-91job就业调查截图";
+
     public static void main(String[] args) {
-
-        ReadExcel readExcel = new ReadExcel();
-
-        Workbook wb = readExcel.getExcel(filePath);
-
-        if (wb == null)
-            System.out.println("文件读入出错");
-        else {
-            getExcelData(wb);
-        }
-        getCommitFileStudent(dirPath, true);
+        initStudentInfo();
+        getCommitFileStudent(dirPath, false);
         getNoCommitFileStudent();
     }
 
@@ -88,7 +46,7 @@ public class CountNoCommitStudent {
     public static void getNoCommitFileStudent() {
         File file = new File(dirPath);
         System.out.println(file.getName() + "---------未完成名单");
-        studentList.stream().forEach(student -> {
+        STUDENT_LIST.stream().forEach(student -> {
             boolean num = commitFileStudentNumList.contains(student.getStudentNum());
             boolean name = commitFileStudentNameList.contains(student.getStudentName());
             boolean b = name || num;
@@ -144,35 +102,4 @@ public class CountNoCommitStudent {
             f.renameTo(new File(f.getParent() + "/" + newFileName));
         }
     }
-
-    /**
-     * 获取Excel中的数据内容
-     *
-     * @param wb Excel对象
-     * @author Fcant 上午 7:38 2020/5/8/0008
-     */
-    public static void getExcelData(Workbook wb) {
-        Sheet sheet = wb.getSheetAt(0);//读取sheet(从0计数)
-        int rowNum = sheet.getLastRowNum();//读取行数(从0计数)
-        for (int i = 0; i <= rowNum; i++) {
-            Row row = sheet.getRow(i);//获得行
-            int colNum = row.getLastCellNum();//获得当前行的列数
-            Student student = new Student();
-            for (int j = 0; j < colNum; j++) {
-                Cell cell = row.getCell(j);//获取单元格
-                if (j == 0) {
-                    String stuNum = cell.toString();
-                    String newNum = "1" + stuNum.substring(2, 11);
-                    student.setStudentNum(newNum.replaceAll("E", "0"));
-                } else {
-                    student.setStudentName(cell.toString());
-                }
-            }
-            studentNumList.add(student.getStudentNum());
-            STUDENT_NUM_MAP.put(student.getStudentNum(), student);
-            STUDENT_NAME_MAP.put(student.getStudentName(), student);
-            studentList.add(student);
-        }
-    }
-
 }
