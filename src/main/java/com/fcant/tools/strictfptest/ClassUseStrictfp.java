f8f20aa6295b2f1dafc9f19ff160d607c1c6d0e9
Fcant <fcscanf@outlook.com>
2020-06-26 23:16:15 +0800

    Add-关于Strictfp关键字的使用：精确指定的类，接口方法的浮点运算

---
+++ src/main/java/com/fcant/tools/strictfptest/ClassUseStrictfp.java
@@ -0,0 +1,18 @@
+package com.fcant.tools.strictfptest;
+
+/**
+ * ClassUseStrictfp
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcant 下午 23:11 2020/6/26/0026
+ */
+public strictfp class ClassUseStrictfp {
+
+    public  ClassUseStrictfp() {
+    }
+
+    public strictfp void sayHello() {
+        System.out.println("Hello");
+    }
+}
---
+++ src/main/java/com/fcant/tools/strictfptest/NumAdd.java
@@ -0,0 +1,13 @@
+package com.fcant.tools.strictfptest;
+
+/**
+ * NumAdd
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcant 下午 23:03 2020/6/26/0026
+ */
+public strictfp interface NumAdd {
+
+    void f();
+}
