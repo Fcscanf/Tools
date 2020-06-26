a34a38ca1534bd6c4c2eafe437ceeab5e7e8f06c
Fcant <fcscanf@outlook.com>
2020-06-26 23:05:32 +0800

    Add-Java7异常处理资源关闭的优雅示例代码

---
+++ src/test/java/com/fcant/tools/exception7/bean/Resource.java
@@ -0,0 +1,19 @@
+package com.fcant.tools.exception7.bean;
+
+/**
+ * Resource
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcant 下午 22:03 2020/6/26/0026
+ */
+public class Resource implements AutoCloseable {
+    public void sayHello() throws Exception {
+        throw new Exception("Resource throw Exception");
+    }
+
+    @Override
+    public void close() throws Exception {
+        throw new Exception("Close method throw Exception");
+    }
+}
---
+++ src/test/java/com/fcant/tools/exception7/trycatch7/CloseResourceAfterJava7.java
@@ -0,0 +1,35 @@
+package com.fcant.tools.exception7.trycatch7;
+
+import com.fcant.tools.exception7.bean.Resource;
+
+/**
+ * CloseResourceAfterJava7
+ * <p>
+ * encoding:UTF-8
+ *
+ * @author Fcant 下午 22:04 2020/6/26/0026
+ */
+public class CloseResourceAfterJava7 {
+
+    public static void main(String[] args) {
+        try {
+            errorTest();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void errorTest() throws Exception {
+        Resource resource = null;
+        try {
+            resource = new Resource();
+            resource.sayHello();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (resource != null) {
+                resource.close();
+            }
+        }
+    }
+}
